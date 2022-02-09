83cee573736fa258689975f4a1a6a1ae848fab4e
onurtaskirancom <taskiranonur9@gmail.com>
2022-02-09 23:22:34 +0300

    Complete Registration

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,3 @@
-// import * as firebase from 'firebase'
-// import firebase from 'firebase/compat/app';
-// import 'firebase/compat/auth';
-
 import "firebase/auth"
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
